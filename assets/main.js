d7f67bfec9b48502830a918e0fcbb64d72354a8f
lizramos611 <liz.ramos@aim.com>
2022-11-08 19:27:34 -0500

    making changes to api

--- assets/main.js
+++ assets/main.js
@@ -14,8 +14,6 @@ function searchWeather(){
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + getCityInput.value + '&units=imperial&appid=f5974a8f39defa4d231ee2456b263b09')
 .then(response => response.json())
   .then(data => 
-{ return data.list.main.temp};
-
 
   displayWeather= function(data) {
     let {temp, wind, humidity} = data.current;
@@ -23,7 +21,7 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + getCityInput.value
     var tempText = document.getElementById("temp");
     var windText = document.getElementById("wind");
     var humidityText = document.getElementById("humidity");
-    tempText.innerHTML = "Temp:" + data[0].temp;
+    tempText.innerHTML = "Temp:" + data[0].list.main.temp;
   
-    displayWeather();
+    displayWeather(data);
   })};
