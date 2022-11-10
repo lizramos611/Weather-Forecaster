c20ab1ceaf590139ecaef2bde6a6b54bb7508cef
lizramos611 <liz.ramos@aim.com>
2022-11-10 16:49:30 -0500

    added some css grid

--- assets/main.js
+++ assets/main.js
@@ -14,17 +14,27 @@ function searchWeather(){
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + getCityInput.value + '&units=imperial&appid=f5974a8f39defa4d231ee2456b263b09')
 .then(response => response.json())
   .then(data => 
-    
+
 {
-    for(i=0; i<4; i++) {
-    
-    var tempText = document.getElementById("temp");
-    var windText = document.getElementById("wind");
-    var humidityText = document.getElementById("humidity");
-    var icon = document.getElementById("icon");
-    tempText.innerHTML = "Temp: " + data.list[i].main.temp;
-    windText.innerHTML = "Wind: " + data.list[i].wind.speed + "MPH";
-    humidityText.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
-    
+    var tempText = document.getElementsByClassName("list-group-item1");
+    for (var i=0; i<tempText.length; i++) {
+         tempText[i].innerHTML = "Temp: " + data.list[i].main.temp + "°";
     }
-})};
+
+    var windText = document.getElementsByClassName("list-group-item2");
+    for (var i=0; i<windText.length; i++) {
+        windText[i].innerHTML = "Wind: " + data.list[i].wind.speed + "MPH";
+    }
+
+    var humidityText = document.getElementsByClassName("list-group-item3");
+    for (var i=0; i<humidityText.length; i++) {
+        humidityText[i].innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+    }
+
+    var iconText = document.getElementsByClassName("icon");
+    var iconNum = data.list[i].weather.icon;
+    for (var i=0; i<iconText.length; i++) {
+fetch('http://openweathermap.org/img/wn/' + iconNum + '@2x.png');
+
+    
+}})};
