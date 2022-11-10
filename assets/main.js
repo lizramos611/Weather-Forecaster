cdfc8c1ae2650ba16463d9b64e7eb1e718d80044
lizramos611 <liz.ramos@aim.com>
2022-11-09 21:56:10 -0500

    updated index for api

--- assets/main.js
+++ assets/main.js
@@ -10,18 +10,21 @@ function searchWeather(){
    var getCityInput = document.getElementById("searchBox");
     var cityName = document.getElementById ("cityName"); 
         cityName.innerHTML = getCityInput.value;
-       
+    
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + getCityInput.value + '&units=imperial&appid=f5974a8f39defa4d231ee2456b263b09')
 .then(response => response.json())
   .then(data => 
     
-    function displayWeather(data){
-for { let i = 0; i >5; i ++} (
+{
+    for(i=0; i<4; i++) {
     
     var tempText = document.getElementById("temp");
     var windText = document.getElementById("wind");
     var humidityText = document.getElementById("humidity");
-    tempText.innerHTML = "Temp:" + data.list[0].main.temp;
- 
-    )
+    var icon = document.getElementById("icon");
+    tempText.innerHTML = "Temp: " + data.list[i].main.temp;
+    windText.innerHTML = "Wind: " + data.list[i].wind.speed + "MPH";
+    humidityText.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+    
+    }
 })};
